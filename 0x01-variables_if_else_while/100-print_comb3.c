8f26e3ec7bb665a47b2ddccf812fc53231b2d903
Bhongofx <bhongofx@gmail.com>
2023-06-16 17:51:50 -0700

    task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,24 +7,19 @@
  */
 int main(void)
 {
-    int d;
-    int p;
+	int d;
+	int p;
 
-    for (d = '0'; d <= '8'; d++)
-    {
-        for (p = d + 1; p <= '9'; p++)
-        {
-            putchar(d);
-            putchar(p);
-
-            if (d == '8' && p == '9')
-                continue;
-
-            putchar(',');
-            putchar(' ');
-        }
-    }
-
-    putchar('\n');
-    return (0);
+	for (d = '0'; d <= '8'; d++)
+		for (p = d + 1; p <= '9'; p++)
+		{
+			putchar(d);
+			putchar(p);
+			if (d == '8' && p == '9')
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
 }
