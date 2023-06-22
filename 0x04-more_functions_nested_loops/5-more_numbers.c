1aed98ea4f85405dedb5e2dac3091ed6084101f8
Bhongofx <bhongofx@gmail.com>
2023-06-22 14:36:42 -0700

    task #5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * more_numbers - print more numbers
+ * more_numbers - Print a series of numbers multiple times.
  */
-
 void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-			_putchar('1');
-		_putchar(j % 10 + '0');
+				_putchar('0' + (j / 10));
+			_putchar('0' + (j % 10));
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
